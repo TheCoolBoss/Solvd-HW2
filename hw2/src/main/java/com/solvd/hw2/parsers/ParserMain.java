e83224a09df8da1ee38e9591c2b5de052ac2f25f
TheCoolBoss <nguyenst22@up.edu>
2023-06-13 21:44:35 -0700

    Parser updates

--- hw2/src/main/java/com/solvd/hw2/parsers/ParserMain.java
+++ hw2/src/main/java/com/solvd/hw2/parsers/ParserMain.java
@@ -2,49 +2,47 @@ package com.solvd.hw2.parsers;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import javax.xml.bind.JAXBException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
+
+import com.solvd.hw2.jaxb.CustomUnmarshaller;
+import com.solvd.hw2.models.*;
+
 public class ParserMain 
 {
-    //src
-    private static final String FALCON_GANON = "hw2/src/main/resources/xml/clients/FalconGanon.xml";
     private static final String FALCON_GANON_PLAYS = "hw2/src/main/resources/xml/experiments/FalconGanonPlays.xml";
     private static final Logger LOGGER = LogManager.getLogger("Main");
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, URISyntaxException
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, URISyntaxException, JAXBException
     {
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-
-        Document falconGanon = builder.parse(FALCON_GANON);
+        Document falconGanon = builder.parse(FALCON_GANON_PLAYS);
         falconGanon.getDocumentElement().normalize();
 
-        XmlParser falconGanonParser = new XmlParser(falconGanon, "client");
-        falconGanonParser.getElements();
-
+        XmlParser expParser = new XmlParser(falconGanon, "experiment");
         
         XmlValidator playsVal = new XmlValidator("hw2/src/main/resources/xml/xsd/Experiment.xsd");
         LOGGER.info("Validating " + FALCON_GANON_PLAYS);
-        playsVal.check(FALCON_GANON_PLAYS);
-        LOGGER.info("Result of validation: ");
-        if (playsVal.getErrors().size() != 0)
+
+        //Currently having issues with using parser vals so just using JAXB after validation
+        if (playsVal.check(FALCON_GANON_PLAYS))
         {
-            playsVal.getErrors().forEach((SAXParseException saxpe) ->
-            {
-                LOGGER.info(saxpe.getMessage());
-            });
+            CustomUnmarshaller expMaker = new CustomUnmarshaller(Experiment.class, FALCON_GANON_PLAYS);
+            Experiment test = (Experiment) expMaker.unmarshal();
+            LOGGER.info(test.toString());
         }
 
-        else
-        {
-            LOGGER.info("No errors :)");
-        }
 
-        
     }
 }
